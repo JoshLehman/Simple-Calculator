77904177a41542f5fd3797602798fd5f4df4cf07
JoshLehman <51763741+JoshLehman@users.noreply.github.com>
2019-07-27 14:29:41 -0400

    Added functions for all of the arithmetic operations as well as made a function for cin.

--- SimpleCalculator/Calc.cpp
+++ SimpleCalculator/Calc.cpp
@@ -1,12 +1,17 @@
 #include<iostream>
 #include<string>
 
-
 double num1, num2;
 void Choose(double num1, double num2, char choice);
 void Log(std::string str);
 void Log(double r);
+double LogIn(double &r);
+char LogIn(char &c);
 void DisplayMenu();
+void Addition();
+void Subtraction();
+void Division();
+void Multiplication();
 char choice;
 
 int main()
@@ -20,47 +25,23 @@ void Choose(double num1, double num2, char choice)
 
 	do
 	{
-		std::cin >> choice;
+		LogIn(choice);
 
 		if (choice == '+')
 		{
-			Log("First number");
-			std::cin >> num1;
-			Log("Second number");
-			std::cin >> num2;
-			double result = num1 + num2;
-			Log(result);
-			Log("Press y to continue or q to quite.");
+			Addition();
 		}
 		else if (choice == '-')
 		{
-			Log("First number");
-			std::cin >> num1;
-			Log("Second number");
-			std::cin >> num2;
-			double result = num1 - num2;
-			Log(result);
-			Log("Press y to continue or q to quite.");
+			Subtraction();
 		}
 		else if (choice == '/')
 		{
-			Log("First number");
-			std::cin >> num1;
-			Log("Second number");
-			std::cin >> num2;
-			double result = num1 / num2;
-			Log(result);
-			Log("Press y to continue or q to quite.");
+			Division();
 		}
 		else if (choice == '*')
 		{
-			Log("First number");
-			std::cin >> num1;
-			Log("Second number");
-			std::cin >> num2;
-			double result = num1 * num2;
-			Log(result);
-			Log("Press y to continue or q to quite.");
+			Multiplication();
 		}
 		else if (choice == 'y')
 		{
@@ -73,6 +54,71 @@ void Choose(double num1, double num2, char choice)
 	} while (choice != 'q' || choice == 'y');
 }
 
+void DisplayMenu()
+{
+	Log("Choose what arithmetic operation you want to use and then pick two numbers.");
+	Log("1. + for Addition");
+	Log("2. - for Subtraction");
+	Log("3. / for Division");
+	Log("4. * for Multiplication");
+}
+
+void Addition()
+{
+	Log("First number");
+	LogIn(num1);
+	Log("Second number");
+	LogIn(num2);
+	double result = num1 + num2;
+	Log(result);
+	Log("Press y to continue or q to quite.");
+}
+
+void Subtraction()
+{
+	Log("First number");
+	LogIn(num1);
+	Log("Second number");
+	LogIn(num2);
+	double result = num1 - num2;
+	Log(result);
+	Log("Press y to continue or q to quite.");
+}
+
+void Division()
+{
+	Log("First number");
+	LogIn(num1);
+	Log("Second number");
+	LogIn(num2);
+	double result = num1 / num2;
+	Log(result);
+	Log("Press y to continue or q to quite.");
+}
+
+void Multiplication()
+{
+	Log("First number");
+	LogIn(num1);
+	Log("Second number");
+	LogIn(num2);
+	double result = num1 * num2;
+	Log(result);
+	Log("Press y to continue or q to quite.");
+}
+
+double LogIn(double &r)
+{
+	std::cin >> r;
+	return r;
+}
+
+char LogIn(char &c)
+{
+	std::cin >> c;
+	return c;
+}
+
 void Log(std::string str)
 {
 	std::cout << str << std::endl;
@@ -81,13 +127,4 @@ void Log(std::string str)
 void Log(double r)
 {
 	std::cout << r << std::endl;
-}
-
-void DisplayMenu()
-{
-	Log("Choose what arithmetic operation you want to use and then pick two numbers.");
-	Log("1. + for Addition");
-	Log("2. - for Subtraction");
-	Log("3. / for Division");
-	Log("4. * for Multiplication");
 }
